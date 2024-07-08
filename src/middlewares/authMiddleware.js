a44897726cee2001b697df23eb6c1a81ee814a67
Nicanor Kyamba <nicanorkyamba98@gmail.com>
2024-07-08 12:12:13 +0300

    feat: Updated auth middleware to take bearer part

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -2,10 +2,13 @@ const jwt = require('jsonwebtoken');
 const { JWT_SECRET } = require('../config');
 
 const authMiddleware = (req, res, next) => {
-    const token = req.header('Authorization');
-    if (!token) {
+    const authHeader = req.header('Authorization');
+
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
         return res.status(401).json({ message: 'Authorization denied' });
     }
+
+    const token = authHeader.split(' ')[1];
 
     try {
         const decoded = jwt.verify(token, JWT_SECRET);
